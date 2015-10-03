3f600d3cd56b83083258b2fdf9782b665f616212
ChenGang <cgangEE@gmail.com>
2015-10-03 12:12:14 +0800

    .

---
+++ cg_head.h
@@ -0,0 +1,28 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <cctype>
+
+#define rep(i, n) for (int i=0; i<(n); ++i)
+#define repf(i, a, b) for (int i=(a); i<=(b); ++i)
+#define repd(i, a, b) for (int i=(a); i>=(b); --i)
+#define sz(a) ((int)(a).size())
+#define SQR(x) ((x)*(x))
+
+using namespace std;
+
+template <class T> void checkmin(T &a, T b){ if (b<a) a=b; }
+#endif
